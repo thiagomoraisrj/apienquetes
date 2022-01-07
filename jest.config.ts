7d31da9ac3441c95319162faedc387d4f52ea812
Thiago Morais <thiago.morais@sellbie.com.br>
2022-01-07 11:02:52 -0300

    chore: adicionar o jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
