d44b4d759f63bf9cfbbbf826634a9e1701bad5e2
Thiago Morais <thiago.morais@sellbie.com.br>
2022-01-11 12:00:17 -0300

    chore: adicionar o husky e o list-staged

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -11,6 +11,6 @@ describe('SignUp Controller', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.statusCode).toBe(200)
   })
 })
